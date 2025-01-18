210afc6d567cca636765c281f3ac51e94c597ab7
Thiago <diasthiago@protonmail.com>
2025-01-17 22:39:33 -0300

    sm6150-common: build audio.primary.default
    will reduce logs

--- common.mk
+++ common.mk
@@ -119,6 +119,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
     audio.a2dp.default \
     audio.bluetooth.default \
+    audio.primary.default \
     audio.primary.sm6150 \
     audio.r_submix.default \
     audio.usb.default \
