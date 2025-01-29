fdfa5804a8d6cfcd77d05688b6b96bfca9d7baa9
Thiago <diasthiago@protonmail.com>
2025-01-29 08:37:30 -0300

    Revert "switch to neutron-clang"
    This reverts commit 86d69e5097376b57860b65eff8e86c6ad4bfb99d.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -49,19 +49,15 @@ BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 co
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := proton
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/motorola/sm6150
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     LLVM=1 \
     LLVM_IAS=1
-
-# Neutron Clang
-TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := neutron
-TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-neutron
-KERNEL_SUPPORTS_LLVM_TOOLS := true
-KERNEL_FULL_LLVM := true
 
 # Kernel modules - Audio
 TARGET_MODULE_ALIASES += \
